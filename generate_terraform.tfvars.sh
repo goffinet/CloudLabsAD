607b6c8f1d07728c31ccfde42fe56ff8471233e6
goffinet <goffinet@goffinet.org>
2024-02-02 20:43:33 +0100

    2024 fix

---
+++ generate_terraform.tfvars.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+PUBLIC_IP=$(curl -s ipinfo.io/ip)
+
+cat << EOF > Terraform/terraform.tfvars
+# Terraform variables
+# See 'variables.tf' for definitions
+
+# Required
+resource-group       = "cloudlab"
+ip-whitelist         = ["${PUBLIC_IP}/32"]
+
+# Optional (defaults are shown)
+timezone             = "W. Europe Standard Time"
+domain-name-label    = "cloudlabs"
+domain-dns-name      = "cloud.labs"
+windows-user         = "labadmin"
+linux-user           = "labadmin"
+hackbox-hostname     = "hackbox"
+elastic-hostname     = "elastic"
+dc-hostname          = "dc"
+winserv2019-hostname = "winserv2019"
+win10-hostname       = "win10"
+win10-size           = "Standard_B4ms"
+winserv2019-size     = "Standard_B4ms"
+dc-size              = "Standard_B4ms"
+elastic-size         = "Standard_B4ms"
+hackbox-size         = "Standard_B4ms"
+EOF
+
+cat Terraform/terraform.tfvars
